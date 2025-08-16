103e39b9619667b9151fd1f24f0f52aac53a286b
tot3110 <abasit3110@gmail.com>
2025-08-16 23:03:07 +0500

    Add About Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from '../Components/NavBar'
 import Header from '../Components/Header'
+import About from '../Components/About'
 
 const App = () => {
   return (
     <div>
       <NavBar />
       <Header />
+      <About />
     </div>
   )
 }
